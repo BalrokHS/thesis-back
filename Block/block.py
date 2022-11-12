a204552e512c1c66cfdf36542811dd45c772ea7c
Lianos Konstantinos <LianosK@unisystems.gr>
2022-11-12 18:00:49 +0200

    Addition in Block methods

--- Block/block.py
+++ Block/block.py
@@ -1,16 +1,26 @@
 import json
+import sys
 from hashlib import sha256
+
+from Transaction import Transaction
 
 
 class Block:
-    def __init__(self, index: int, transactions: list, timestamp: float, previous_hash: str):
-        self.index = index
-        self.transactions = transactions
-        self.timestamp = timestamp
-        self.previous_hash = previous_hash
+    def __init__(self, index: int, transactions: [], timestamp: float, previous_hash: str):
+        self.index: int = index
+        self.transactions: [] = transactions
+        self.timestamp: float = timestamp
+        self.previous_hash: str = previous_hash
 
     # Generates a hash based on the current state of the block
+    @property
     def current_hash(self) -> str:
         block_string = json.dumps(self.__dict__, sort_keys=True)
         return sha256(block_string.encode()).hexdigest()
 
+    def add_transaction(self, tx: Transaction) -> None:
+        self.transactions.append(tx)
+
+    @property
+    def get_size_of_block(self) -> int:
+        return sys.getsizeof(self.transactions)
