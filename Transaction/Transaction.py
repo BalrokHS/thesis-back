c377fe5b4a2fb1877a40b4af42e18942ab486d81
Lianos Konstantinos <LianosK@unisystems.gr>
2022-11-12 17:59:59 +0200

    Augment transaction.py logic + addition the transaction package

--- Transaction/Transaction.py
+++
@@ -1,2 +0,0 @@
-class Transaction:
-    def __init__(self, data, ):
---
+++ Transaction/transaction.py
@@ -0,0 +1,5 @@
+class Transaction:
+    def __init__(self, sender: str, reciever: str, data):
+        self.sender = sender
+        self.reciever = reciever
+        self.data = data
