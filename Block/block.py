98ab9a52a330dcc10706954f6ce5a7a9304e3c80
Lianos Konstantinos <kostas.lianos.96@gmail.com>
2023-01-05 03:14:43 +0200

    Append transaction in block file

--- Block/block.py
+++ Block/block.py
@@ -1,8 +1,13 @@
+import json
 import sys
 from hashlib import sha256
 
 from Transaction import Transaction
-from models import BlockSchema
+from models import BlockSchema, TransactionSchema
+
+
+def block_dir(x) -> str:
+    return "data/{}.json".format(x)
 
 
 class Block:
@@ -12,15 +17,24 @@ class Block:
         self.timestamp: float = timestamp
         self.previous_hash: str = previous_hash
 
+        # Create Block file for persistence
+        with open(block_dir(index), 'r+') as file:
+            json.dump([], file)
+
     # Generates a hash based on the current state of the block
     @property
     def current_hash(self) -> str:
         block_string = BlockSchema().dumps(self)
-        print(block_string)
         return sha256(block_string.encode()).hexdigest()
 
     def add_transaction(self, tx: Transaction) -> None:
-        self.transactions.append(tx)
+        with open(block_dir(self.index), 'r') as file:
+            file_data = json.load(file)
+            file_data.append(TransactionSchema().dump(tx))
+
+        with open(block_dir(self.index), 'w') as file:
+            json.dump(file_data, file, indent=2)
+            self.transactions.append(tx)
 
     @property
     def get_size_of_block(self) -> int:
